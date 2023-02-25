c216ecd8cd34465db32dfa7274950357d0a54885
Cult of Zuiun <hesitantray2@gmail.com>
2023-02-25 01:13:58 -0600

    Temporary takedown while I sort out sourcing

--- main.js
+++ main.js
@@ -1,31 +1,7 @@
 /*
- * Gets current page file path
- *
- * pre: none
- * post: none
- * return: string = file path
- */
-function get_file_path () {
-    return window.location.pathname;
-}
-
-/*
- * Converts HTML path to JSON path
- *
- * path: string = path to HTML file
- * 
- * pre: none
- * post: none
- * return: string = path to JSON file
- */
-function convert_html_json (path) {
-    return path.replace ("html", "json");
-}
-
-/*
  * Imports a file
  *
- * path: string = path to file
+ * path: String = path to file
  * 
  * pre: none
  * post: none
@@ -33,13 +9,13 @@ function convert_html_json (path) {
  */
 async function import_file (path) {
     return await fetch (path)
-        .catch((error) => console.log ("Error: " + error));
+        .catch((error) => console.log ("File Import Error: " + error));
 }
 
 /*
  * Imports a file as a JSON
  *
- * path: string = path to file
+ * path: String = path to file
  * 
  * pre: none
  * post: none
@@ -48,8 +24,135 @@ async function import_file (path) {
 async function import_json (path) {
     return await import_file (path)
         .then ((response) => response.json ())
-        .catch((error) => console.log ("Error: " + error));
+        .catch((error) => console.log ("JSON Import Error: " + error));
 }
+
+/*
+ * Builds skeleton page (header and footer)
+ *
+ * pre: none
+ * post: none
+ * return: none
+ */
+async function build_skeleton () {
+    let file = await import_file (window.location.pathname);
+    let template = await import_json ("/template.json");
+    let head_element = document.getElementsByTagName ("head") [0];
+    let body_element = document.getElementsByTagName ("body") [0];
+
+    body_element.innerHTML = template ["header"].join ("") + body_element.innerHTML + template ["footer"].join ("");
+
+    // Add scroll button if necessary
+    if (document.body.clientWidth < window.innerWidth) { // There is a scroll bar
+        let aside_element = document.getElementsByTagName ("aside") [1];
+
+        aside_element.innerHTML = template ["scroll"].join ("");
+    }
+
+    // Use normal page
+    if (file.ok) {
+        head_element.innerHTML = head_element.innerHTML + template ["favicons"].join ("");
+    // Use 404 page
+    } else {
+        /* let title_element = document.getElementById ("title");
+
+        head_element.innerHTML = head_element.innerHTML + template ["404"] [0].join ("");
+        title_element.innerHTML = template ["404"] [1].join (""); */
+    }
+
+    // Fade header as page is scrolled
+    window.onscroll = function () {
+        let header = document.getElementsByTagName ("header") [0];
+        let bounds = document.getElementsByTagName ("hr") [0].getBoundingClientRect ();
+
+        header.style.opacity = Math.max (bounds.bottom / (bounds.bottom + window.pageYOffset), 0.5);
+    }
+}
+
+/*
+ * Builds index page
+ *
+ * pre: none
+ * post: none
+ * return: none
+ */
+async function build_index () {
+    let file = await import_file (window.location.pathname);
+    let template = await import_json ("/template.json");
+    let head_element = document.getElementsByTagName ("head") [0];
+    let body_element = document.getElementsByTagName ("body") [0];
+
+    body_element.innerHTML = template ["header"].join ("") + body_element.innerHTML + template ["footer"].join ("");
+
+    // Add scroll button if necessary
+    if (document.body.clientWidth < window.innerWidth) {
+        let aside_element = document.getElementsByTagName ("aside") [1];
+
+        aside_element.innerHTML = template ["scroll"].join ("");
+    }
+
+    // Use normal page
+    if (file.ok) {
+        head_element.innerHTML = head_element.innerHTML + template ["head"].join ("");
+    // Use 404 page
+    } else {
+        let title_element = document.getElementById ("title");
+
+        head_element.innerHTML = head_element.innerHTML + template ["404"] [0].join ("");
+        title_element.innerHTML = template ["404"] [1].join ("");
+    }
+
+    // Fade header as page is scrolled
+    window.onscroll = function () {
+        let header = document.getElementsByTagName ("header") [0];
+        let bounds = document.getElementsByTagName ("hr") [0].getBoundingClientRect ();
+
+        header.style.opacity = Math.max (bounds.bottom / (bounds.bottom + window.pageYOffset), 0.5);
+    }
+}
+
+/*
+ * Builds article page
+ *
+ * pre: none
+ * post: none
+ * return: none
+ */
+async function build_article () {
+    let file = await import_file (window.location.pathname);
+    let template = await import_json ("/template.json");
+    let head_element = document.getElementsByTagName ("head") [0];
+    let body_element = document.getElementsByTagName ("body") [0];
+
+    body_element.innerHTML = template ["header"].join ("") + body_element.innerHTML + template ["footer"].join ("");
+
+    // Add scroll button if necessary
+    if (document.body.clientWidth < window.innerWidth) {
+        let aside_element = document.getElementsByTagName ("aside") [1];
+
+        aside_element.innerHTML = template ["scroll"].join ("");
+    }
+
+    // Use normal page
+    if (file.ok) {
+        head_element.innerHTML = head_element.innerHTML + template ["head"].join ("");
+    // Use 404 page
+    } else {
+        let title_element = document.getElementById ("title");
+
+        head_element.innerHTML = head_element.innerHTML + template ["404"] [0].join ("");
+        title_element.innerHTML = template ["404"] [1].join ("");
+    }
+
+    // Fade header as page is scrolled
+    window.onscroll = function () {
+        let header = document.getElementsByTagName ("header") [0];
+        let bounds = document.getElementsByTagName ("hr") [0].getBoundingClientRect ();
+
+        header.style.opacity = Math.max (bounds.bottom / (bounds.bottom + window.pageYOffset), 0.5);
+    }
+}
+
 
 /*
  * Builds page
@@ -59,37 +162,12 @@ async function import_json (path) {
  * return: none
  */
 async function build_page () {
-    let file = await import_file (get_file_path ());
-    let template_json = await import_json ("/template.json");
-    let head_element = document.getElementsByTagName ("head") [0];
-    let body_element = document.getElementsByTagName ("body") [0];
+    let file = await import_file (window.location.pathname.replace ("html", "json"));
 
-    body_element.innerHTML = template_json ["header"].join ("") + body_element.innerHTML + template_json ["footer"].join ("");
+    build_skeleton ();
 
-    // Add scroll button if necessary
-    if (document.body.clientWidth < window.innerWidth) {
-        let aside_element = document.getElementsByTagName ("aside") [1];
+    if (file [file.length - 1]) {
 
-        aside_element.innerHTML = template_json ["scroll"].join ("");
-    }
-
-    // Use normal page
-    if (file.ok) {
-        head_element.innerHTML = head_element.innerHTML + template_json ["head"].join ("");
-    // Use 404 page
-    } else {
-        let title_element = document.getElementById ("title");
-
-        head_element.innerHTML = head_element.innerHTML + template_json ["404"] [0].join ("");
-        title_element.innerHTML = template_json ["404"] [1].join ("");
-    }
-
-    // Fade header as page is scrolled
-    window.onscroll = function () {
-        let header = document.getElementsByTagName ("header") [0];
-        let bounds = document.getElementsByTagName ("hr") [0].getBoundingClientRect ();
-
-        header.style.opacity = Math.max (bounds.bottom / (bounds.bottom + window.pageYOffset), 0.5);
     }
 }
 
