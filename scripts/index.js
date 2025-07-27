02c7a3aba44f20226299e9b2539bbd60f5204948
Cult of Zuiun <hesitantray2@gmail.com>
2025-07-27 12:44:16 -0500

    Removed dynamic index population

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,3 @@
-import {import_json} from "/modules/utilities.mjs"
-
-/*
- * Builds article panel
- *
- * article: JSON = JSON of article
- *
- * Pre: None
- * Post: None
- * Return: HTMLAnchorElement = Article panel
- */
-function build_articles_panel (article) {
-    let articles_panel = document.createElement ("a");
-    let figure = document.createElement ("figure");
-    let img = document.createElement ("img");
-    let figcaption = document.createElement ("figcaption");
-
-    /*
-     * "<a href = \"LINK\" class = \"articles_panel\">",
-     *     "<figure>",
-     *         "<img src = \"COVER\" alt = \"CAPTION\" title = \"CAPTION\">",
-     *         "<figcaption>TITLE</figcaption>",
-     *     "</figure>",
-     * "</a>"
-     */
-    articles_panel.classList.add ("articles_panel");
-    articles_panel.href = article ["link"];
-
-    if ("cover" in article) {
-        img.src = article ["cover"] [0];
-        img.alt = img.title = article ["cover"] [1];
-    }
-
-    figcaption.innerHTML = article ["title"];
-
-    if ("cover" in article) {
-        figure.appendChild (img);
-    }
-
-    figure.appendChild (figcaption);
-    articles_panel.appendChild (figure);
-    return articles_panel;
-}
-
 /*
  * Builds article index page
  *
@@ -50,15 +6,8 @@ function build_articles_panel (article) {
  * Return: None
  */
 async function build_index () {
-    let articles = await import_json ("/data/articles.json");
     let container = document.getElementById ("articles");
-    let count = 0;
-
-    // Populate container with appropriate panels
-    for (let i of Object.values (articles [ARTICLES_TYPE])) {
-        container.appendChild (build_articles_panel (await import_json (i)));
-        count ++;
-    }
+    let count = container.children.length;
 
     // Three-align container
     for (let i = count; i % 3 !== 0; i ++) {
